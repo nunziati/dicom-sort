60b77b6ae5de50f92a781f62b69824b44b7b4112
Giacomo Nunziati <72670358+nunziati@users.noreply.github.com>
2024-10-17 17:09:24 +0200

    correct path

--- script/unique_series_description.py
+++ script/unique_series_description.py
@@ -3,18 +3,11 @@ from pathlib import Path
 
 from dicomselect import Database
 
-root = Path("/data/pelvis/projects/giacomo")
+root = Path("/data/pelvis/projects/dicom_datasets/giacomo")
 
 dataset_path = root / "dicom_dataset" / "RUMC"
 
 db_path = Path('./')
-
-# Clean the old db files
-if db_path.exists():
-    os.remove(db_path / 'archive.log')
-    os.remove(db_path / 'archive.progress')
-    os.remove(db_path / 'archive.pending.db')
-
 
 db = Database(db_path / "archive.db")
 db.create(dataset_path, max_workers=4, max_rows=1000)
